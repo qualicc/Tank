0d433cf7d1d102e71d8cac7fa8403e7e2f72f0d3
Jakub Moszyński <lolin515@wp.pl>
2023-04-06 15:37:51 +0200

    06.04.4

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const displayArray = document.querySelector("#displayArray > pre");
 const canvas = document.querySelector("#canvas");
 const scoreElement = document.querySelector("#score");
 const ctx = canvas.getContext("2d");
+document.addEventListener("keydown",onKeyDown,);
 
 
 const rozmiarPola = 30;
@@ -27,27 +28,112 @@ class Player {
         return this.y;
     }
     moveLeft() {
-        if (this.x - rozmiarPola >= 0) {
-          this.x -= rozmiarPola;
+        if (this.x > 0) {
+          this.x -= rozmiarPola/2;
           this.kierunek = 4;
         }
     }
     moveRight() {
         if (this.x + rozmiarPola < canvas.width) {
-          this.x += rozmiarPola;
+          this.x += rozmiarPola/2;
           this.kierunek = 2;
         }
      }
     moveUp() {
-        if (this.y - rozmiarPola >= 0) {
-          this.y -= rozmiarPola;
-          this.kierunek = 1;
+        if (this.y > 0) {
+          this.y -= rozmiarPola/2;
+          this.kierunek = 3;
         }
     }
     moveDown() {
         if (this.y + rozmiarPola < canvas.height) {
-          this.y += rozmiarPola;
+          this.y += rozmiarPola/2;
+          this.kierunek = 1;
+        }
+    }
+    playerDraw(){
+        var color = "red";
+        switch(this.kierunek){
+            case 1:      
+                ctx.fillStyle = "#494646";
+                ctx.fillRect(this.x,this.y,5,rozmiarPola); //gąska1
+                ctx.fillRect(this.x+25,this.y,5,rozmiarPola); //gąska2
+                ctx.fillStyle = color;
+                ctx.fillRect(this.x+5,this.y+3,20,22); //kadłub
+                ctx.fillStyle = "#d2ced1";
+                ctx.fillRect(this.x+13,this.y+15,3,15); //działo
+                ctx.fillStyle = "#494646";
+                ctx.fillRect(this.x+9,this.day+9,12,12);  //wieża
+           break;
+    
+            case 2:
+                ctx.fillStyle = "#494646";
+                ctx.fillRect(this.x,this.y,rozmiarPola,5); //gąska1
+                ctx.fillRect(this.x,this.y+25,rozmiarPola,5); //gąska2
+                ctx.fillStyle = color;
+                ctx.fillRect(this.x+3,this.y+5,22,20); //kadłub
+                ctx.fillStyle = "#d2ced1";
+                ctx.fillRect(this.x+15,this.y+13,15,3); //działo
+                ctx.fillStyle = "#494646";
+                ctx.fillRect(this.x+9,this.y+9,12,12); //wieża
+            break;
+    
+            case 3:
+                ctx.fillStyle = "#494646";
+                ctx.fillRect(this.x,this.y,5,rozmiarPola); //gąska1
+                ctx.fillRect(this.x+25,this.y,5,rozmiarPola); //gąska2
+                ctx.fillStyle = color;
+                ctx.fillRect(this.x+5,this.y+5,20,22); //kadłub
+                ctx.fillStyle = "#d2ced1";
+                ctx.fillRect(this.x+13,this.y,3,15); //działo
+                ctx.fillStyle = "#494646";
+                ctx.fillRect(this.x+9,this.y+9,12,12); //wieża
+            break;
+    
+            case 4:
+                ctx.fillStyle = "#494646";
+                ctx.fillRect(this.x,this.y,rozmiarPola,5); //gąska1
+                ctx.fillRect(this.x,this.y+25,rozmiarPola,5); //gąska2
+                ctx.fillStyle = color;
+                ctx.fillRect(this.x+5,this.y+5,22,20); //kadłub
+                ctx.fillStyle = "#d2ced1";
+                ctx.fillRect(this.x,this.y+13,15,3); //działo
+                ctx.fillStyle = "#494646";
+                ctx.fillRect(this.x+9,this.y+9,12,12); //wieża
+            break;
+    
+        }
+    }
+}
+class Bot{
+    x;
+    y;
+    constructor(posx, posy){
+        this.x = posx;
+        this.y = posy;
+    }
+    moveLeft() {
+        if (this.x > 0) {
+          this.x -= rozmiarPola/2;
+          this.kierunek = 4;
+        }
+    }
+    moveRight() {
+        if (this.x + rozmiarPola < canvas.width) {
+          this.x += rozmiarPola/2;
+          this.kierunek = 2;
+        }
+     }
+    moveUp() {
+        if (this.y > 0) {
+          this.y -= rozmiarPola/2;
           this.kierunek = 3;
+        }
+    }
+    moveDown() {
+        if (this.y + rozmiarPola < canvas.height) {
+          this.y += rozmiarPola/2;
+          this.kierunek = 1;
         }
     }
     playerDraw(){
@@ -67,7 +153,7 @@ class Player {
     
             case 2:
                 ctx.fillStyle = "#494646";
-                ctx.fillRect(this.x,y,rozmiarPola,5); //gąska1
+                ctx.fillRect(this.x,this.y,rozmiarPola,5); //gąska1
                 ctx.fillRect(this.x,this.y+25,rozmiarPola,5); //gąska2
                 ctx.fillStyle = color;
                 ctx.fillRect(this.x+3,this.y+5,22,20); //kadłub
@@ -107,10 +193,12 @@ class Player {
 class Game {
     map;
     player = new Player();
+    bots = [];
     constructor(lvl) {
         this.map = this.loadMap(lvl);
+        // $("canvas").css("height" , this.map.length * rozmiarPola);
+        // $("canvas").css("widht" , this.map[1].length * rozmiarPola);
     }
-    
     loadMap(lvl) {
         let map = null;
         $.ajax({
@@ -146,6 +234,7 @@ class Game {
                         ctx.fillRect(i*rozmiarPola,j*rozmiarPola,rozmiarPola,rozmiarPola);
                         break;               
                     case 4:
+                        this.bots.push(new Bot(i*rozmiarPola,j*rozmiarPola))
                         ctx.fillStyle = "grey";//boty
                         ctx.fillRect(i*rozmiarPola,j*rozmiarPola,rozmiarPola,rozmiarPola);
                         break;
@@ -168,47 +257,48 @@ class Game {
     update(){
         this.clear()
         this.draw()
-        this.move()
         this.player.playerDraw()
-        
     }
-    
-    move() {
-        if (this.player.actualStep < stepWait) {
-          this.player.actualStep++;
-          return;
-        }
-      
-        const self = this; // zapisanie referencji do obiektu klasy Game w zmiennej self
-      
-        window.addEventListener('keyup', function(event) {
-          switch(event.keyCode) {
-            case 65: // lewa strzałka
-              console.log(self.player.x + " " + self.player.y)
-              self.player.moveLeft();
+    move(kierunek) {
+          switch(kierunek) {
+            case 1: // lewa strzałka
+              console.log(this.player.x + " " + this.player.y)
+              this.player.moveLeft();
               break;
-            case 87: // górna strzałka
-              console.log(self.player.x + " " + self.player.y)
-              self.player.moveUp();
+            case 2: // górna strzałka
+              console.log(this.player.x + " " + this.player.y)
+              this.player.moveUp();
               break;
-            case 68: // prawa strzałka
-              console.log(self.player.x + " " + self.player.y)
-              self.player.moveRight();
+            case 3: // prawa strzałka
+              console.log(this.player.x + " " + this.player.y)
+              this.player.moveRight();
               break;
-            case 83: // dolna strzałka
-              console.log(self.player.x + " " + self.player.y)
-              self.player.moveDown();
+            case 4: // dolna strzałka
+              console.log(this.player.x + " " + this.player.y)
+              this.player.moveDown();
               break;
-          }
-        });
-      
-        this.player.actualStep = 0;
-      }
-      
-
+        }      
+    }
 }
 
 let one = new Game("test");
 one.draw();
 
-let updateInterval = setInterval(one.update.bind(one), 1000);
+let updateInterval = setInterval(one.update.bind(one), 10);
+
+function onKeyDown(event){
+    switch (event.code) {
+        case "KeyA":
+            one.move(1)
+            break;
+        case "KeyW":
+            one.move(2)
+            break;
+        case "KeyD":
+            one.move(3)
+            break;
+        case "KeyS":
+            one.move(4)
+            break;    
+    }
+} 
